f62b875d386e00e217074a5183402d6b6764e024
Zachary Becker <zrbecker@gmail.com>
2025-06-06 21:44:10 -0400

    do copy paste for usage instead

--- .github/scripts/docker_vars.sh
+++ .github/scripts/docker_vars.sh
@@ -1,23 +1,6 @@
 #!/bin/sh
 set -eu
 
-while [ "$#" -gt 0 ]; do
-    case "$1" in
-    --ns)
-        DOCKER_NS="$2"
-        shift 2
-        ;;
-    --repo)
-        DOCKER_REPO="$2"
-        shift 2
-        ;;
-    *)
-        echo "Unknown option: $1" >&2
-        exit 1
-        ;;
-    esac
-done
-
 : "${DOCKER_NS:?--ns is required}"
 : "${DOCKER_REPO:?--repo is required}"
 : "${GITHUB_REF:?GITHUB_REF must be set}"
@@ -27,30 +10,29 @@ echo "DOCKER_NS=${DOCKER_NS}"
 echo "DOCKER_REPO=\"${DOCKER_REPO}\""
 echo "DOCKER_REPO_PATH=${DOCKER_NS}/${DOCKER_REPO}"
 
+PUSH="false"
 TAGS=""
-global_prefix="${DOCKER_TAG_PREFIX:-}"
 global_suffix="${DOCKER_TAG_SUFFIX:-}"
 if printf '%s\n' "$GITHUB_REF" | grep -q '^refs/heads/'; then
     sanitized=$(echo "$GITHUB_REF_NAME" | sed 's/[^a-zA-Z0-9_.-]/-/g')
-    branch_prefix="${DOCKER_TAG_BRANCH_PREFIX:-}"
-    branch_suffix="${DOCKER_TAG_BRANCH_SUFFIX:-}"
-
-    base_tag="${branch_prefix}${sanitized}${branch_suffix}"
-    full_tag="${global_prefix}${base_tag}${global_suffix}"
-    TAGS="$full_tag"
+    TAGS="${sanitized}${global_suffix}"
 
     # TODO(zrbecker): it's common to set main to latest, but not ideal as this should actually be the latest release tag
     if [ "$GITHUB_REF_NAME" = "main" ]; then
-        latest_tag="${global_prefix}latest${global_suffix}"
+        latest_tag="latest${global_suffix}"
         TAGS="${TAGS},${latest_tag}"
     fi
+
+    PUSH="true"
 elif printf '%s\n' "$GITHUB_REF" | grep -q '^refs/tags/'; then
     sanitized=$(echo "$GITHUB_REF_NAME" | sed 's/[^a-zA-Z0-9_.-]/-/g')
-    TAGS="${global_prefix}${sanitized}${global_suffix}"
+    TAGS="${sanitized}${global_suffix}"
+    PUSH="true"
 else
     TAGS=""
 fi
 echo "DOCKER_TAGS=${TAGS}"
+echo "DOCKER_PUSH=${PUSH}"
 
 REFS=""
 if [ -n "$TAGS" ]; then
